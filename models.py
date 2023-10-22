9c885ce51915cffe86c3064fe6c14a9aa8b3a49a
Jimmy Aronsson <jimmy_aronsson@outlook.com>
2023-10-22 10:51:43 +0200

    Add small LSTM model

--- models.py
+++ models.py
@@ -1,13 +1,13 @@
 import torch.nn as nn
 
 
-class ModelLSTM(nn.Module):
+class LargeLSTM(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.nlayers = 10
+        self.nlayers = 15
         self.input_size = 4
-        self.hidden_size = 100
+        self.hidden_size = 200
 
         self.lstm1 = nn.LSTM(input_size=self.input_size,
                              hidden_size=self.hidden_size,
@@ -37,7 +37,9 @@ class ModelLSTM(nn.Module):
                                stride=1
                                )
 
-        self.linear = nn.Linear(self.hidden_size, 4)
+        self.linear1 = nn.Linear(self.hidden_size, 100)
+        self.linear2 = nn.Linear(100, 200)
+        self.linear3 = nn.Linear(200, 4)
         self.activation = nn.ReLU()
 
     def forward(self, x):
@@ -49,6 +51,75 @@ class ModelLSTM(nn.Module):
         x = self.activation(x)
         x = self.conv2(x.permute(0, 2, 1)).permute(0, 2, 1)
 
-        x = self.linear(x)
+        x = self.activation(x)
+        x = self.linear1(x)
+
+        x = self.activation(x)
+        x = self.linear2(x)
+
+        x = self.activation(x)
+        x = self.linear3(x)
+
+        return x
+
+
+class SmallLSTM(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.nlayers = 2
+        self.input_size = 4
+        self.hidden_size = 10
+
+        self.lstm1 = nn.LSTM(input_size=self.input_size,
+                             hidden_size=self.hidden_size,
+                             num_layers=self.nlayers,
+                             batch_first=True,
+                             )
+
+        self.lstm2 = nn.LSTM(input_size=self.hidden_size,
+                             hidden_size=self.hidden_size,
+                             num_layers=self.nlayers,
+                             batch_first=True,
+                             )
+
+        self.conv1 = nn.Conv1d(in_channels=self.hidden_size,
+                               out_channels=self.hidden_size,
+                               kernel_size=9,
+                               dilation=1,
+                               padding=4,
+                               stride=1
+                               )
+
+        self.conv2 = nn.Conv1d(in_channels=self.hidden_size,
+                               out_channels=self.hidden_size,
+                               kernel_size=5,
+                               dilation=1,
+                               padding=2,
+                               stride=1
+                               )
+
+        self.linear1 = nn.Linear(self.hidden_size, 20)
+        self.linear2 = nn.Linear(20, 30)
+        self.linear3 = nn.Linear(30, 4)
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        x, (h, c) = self.lstm1(x)
+        x = self.activation(x)
+        x = self.conv1(x.permute(0, 2, 1)).permute(0, 2, 1)
+
+        x, (_, _) = self.lstm2(x, (h, c))
+        x = self.activation(x)
+        x = self.conv2(x.permute(0, 2, 1)).permute(0, 2, 1)
+
+        x = self.activation(x)
+        x = self.linear1(x)
+
+        x = self.activation(x)
+        x = self.linear2(x)
+
+        x = self.activation(x)
+        x = self.linear3(x)
 
         return x
